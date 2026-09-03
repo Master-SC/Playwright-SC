d14f4ddf4fc23e261ca80a67e5b77e3ec4d5945a
Master-SC <shibashis.20@gmail.com>
2026-09-04 02:49:01 +0530

    modification to typescript

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,7 @@ module.exports = defineConfig({
   // This is to select which browser we will run our test againsts . 
   use:{
     browserName:'chromium',
-    headless:true,
+    headless:false,
     // screenshot:'on',
     // trace:'on',
     // trace:'retain-on-failure'
